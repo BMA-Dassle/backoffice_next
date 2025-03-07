b9d252bcfff7651589f2cd2cd6ee49a226a59566
Alex <alex@dassle.us>
2025-03-07 17:27:53 -0500

    Emergency Patch for Monthly goal Bar

--- src/app/sales/[agent]/dashboard/_components/goalTargetBar.tsx
+++ src/app/sales/[agent]/dashboard/_components/goalTargetBar.tsx
@@ -7,26 +7,10 @@ import { getLiveTotal } from "../_lib/cards/getLiveTotal";
 import dayjs from "dayjs";
 import { getGoalData } from "../_lib/cards/getGoal";
 
-const getGoal = cache(async (agent: string) => {
-  return await getGoalData(agent, dayjs().year(), dayjs().month() + 1);
-});
-
-const getLive = unstable_cache(
-  async (agent: string) => {
-    const live = await getLiveTotal(agent);
-    return {
-      value: live,
-      display: live.format(),
-    };
-  },
-  ["dashboardLiveProgressBar"],
-  { revalidate: 1, tags: ["dashboardLiveProgressBar"] }
-);
-
 export async function GoalTargetBar({ agent }: { agent: string }) {
-  const liveTotal = await getLive(agent);
-  const goalTotal = await getGoal(agent);
-  const livePercent = Math.round((liveTotal.value.intValue / goalTotal.intValue) * 100);
+  const liveTotal = await getLiveTotal(agent);
+  const goalTotal = await getGoalData(agent, dayjs().year(), dayjs().month() + 1);
+  const livePercent = Math.round((liveTotal.intValue / goalTotal.intValue) * 100);
 
   let color = "red.8";
   if (livePercent > 33 && livePercent <= 66) {
@@ -37,7 +21,7 @@ export async function GoalTargetBar({ agent }: { agent: string }) {
 
   return (
     <ProgressRoot size={"100%"}>
-      <Tooltip label={`${liveTotal.display} - ${livePercent}%`}>
+      <Tooltip label={`${liveTotal.format()} - ${livePercent}%`}>
         <ProgressSection value={livePercent} color={color}></ProgressSection>
       </Tooltip>
     </ProgressRoot>
